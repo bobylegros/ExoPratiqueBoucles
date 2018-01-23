6704eee7c4c03d421018644250aa6f57b3afb386
bobylegros <rafcapdeville@gmail.com>
2018-01-23 14:04:43 +0100

    deux

--- app.rb
+++ app.rb
@@ -1,5 +1,43 @@
-hello = "Hello World!"
+def boucle_times
+  hello = "Hello World!"
 
-10.times do
-  puts hello
-end 
+  10.times do
+    puts hello
+  end
+end
+
+# 10.times{puts hello}
+#
+# 10.times do
+#   puts "Hi ma biche"
+# end
+
+
+def boucle_while
+  i = 0
+
+  while i < 10
+      puts "Fred est un ouf"
+
+      i += 1
+    end
+end
+
+def boucle_for
+  for b in 0..10
+      puts "Super exo"
+end
+end
+
+def boucle_each
+  (1..10).each do
+    puts "Tip top ouff"
+  end
+
+end
+
+
+boucle_times
+boucle_while
+boucle_for
+boucle_each
